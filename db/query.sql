a1009cede4d450923acaa8dc6c4c333e8bc48092
chiragkarnawat <49971870+chiragkarnawat@users.noreply.github.com>
2020-05-10 02:01:51 +0530

    upd

--- db/query.sql
+++ db/query.sql
@@ -1,8 +1,8 @@
-select * from ( 
-select case when y.event_name = y.next_event_id and y.next_event_id=y.next_event_id_2 then 1 else 0 end as ind,
+
+select distinct event_name from ( 
+select case when y.event_name = y.next_event_id and y.next_event_id=y.next_event_id_2 and y.event_name = y.next_event_id_2 then 1 else 0 end as ind,
 y.event_name, y.next_event_id,y.next_event_id_2, y.people_count 
 from (
 select z.event_name , z.next_event_id, LEAD(z.event_name) over (ORDER BY z.id) AS next_event_id_2 ,z.people_count from ( 
-select event_name ,  LEAD(event_name) over (ORDER BY id) AS next_event_id,people_count from test.test_table ) z
+select event_name,id ,  LEAD(event_name) over (ORDER BY id) AS next_event_id,people_count from project_loylty.test_Data1  ) z
   )y )x where x.ind=1 and x.people_count>100;
-  
